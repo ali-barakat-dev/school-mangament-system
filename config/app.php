859396bf5e6aca7116c31f3aeb84a06161bdf3d7
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-02 18:59:19 +0200

    change RepoServiceProvider

--- config/app.php
+++ config/app.php
@@ -174,7 +174,7 @@ return [
             // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        App\Providers\ReposerviceProvider::class,
+        App\Providers\RepoServiceProvider::class,
 
 
     ],
