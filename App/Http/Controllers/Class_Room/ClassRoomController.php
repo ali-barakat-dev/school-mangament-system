2f84520445bcf7590dd446dd71d784f4bee51ece
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-05 02:31:05 +0200

    add class room and folder class room

---
+++ App/Http/Controllers/Class_Room/ClassRoomController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers\Class_Room;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ClassRoomRequest;
+use App\Models\Classroom;
+use App\Models\Grade;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+class ClassRoomController extends Controller
+{
+    function index()
+    {
+
+        $clases = ClassRoom::with(['grade'])->select(
+            [
+                'id',
+                'name_class_' . LaravelLocalization::getCurrentLocale() . ' as name',
+                'grade_id',
+            ]
+        )->get();
+      return view('class_room.classroom', compact('clases'));
+    
+    }
+    function create()
+    {
+        $grades = Grade::all();
+        return view('class_room.add_class', compact('grades'));
+    }
+    function store(ClassRoomRequest $request)
+    {
+        Classroom::create([
+            'name_class_ar' => $request['name_class_ar'],
+            'name_class_en' => $request['name_class_en'],
+            'grade_id' => $request['grade_id'],
+        ]);
+        return redirect()->route('class.index');
+    }
+    function edit($class_id)
+    {
+        $grade = Grade::all();
+        $class = ClassRoom::find($class_id);
+        ClassRoom::select([
+            'name_class_ar',
+            'name_class_en',
+            'grade_id',
+        ]);
+        return view('class_room.edit_class', compact('class', 'grade'));
+    }
+    public function update(ClassRoomRequest $request, $class_id)
+    {
+        $class = ClassRoom::find($class_id);
+        if (!$class)
+            return redirect()->back();
+        $class->update($request->all());
+        return redirect()->route('class.index');
+    }
+    public function destroy($class_id)
+    {
+        $class = ClassRoom::find($class_id);
+        $class->delete();
+        return redirect()->route('class.index');
+    }
+
+}
