dd4ad6ba90c08b63fbe18c4ffcddb85bd0f59ecb
ali-barakat-dev <Alli.barakat.dev@gmail.com>
2023-12-05 02:40:02 +0200

    change name function to camelCase

--- App/Repositry/StudentRepositry.php
+++ App/Repositry/StudentRepositry.php
@@ -17,26 +17,34 @@ use Illuminate\Support\Facades\Hash;
 
 interface StudentRepositryInterface
 {
-    public function getAllStudents();
+    public function getAllStudent();
+
     public function getGender();
-    public function StudentsStore($request);
-  public function editStudents($id);
-  public function StudentsUpdate($request);
-  public function DeleteStudents($id);
+
+    public function studentStore($request);
+
+    public function craeteStudent();
+
+    public function editStudent($id);
+
+    public function studentsUpdate($request);
+
+    public function deleteStudent($id);
 }
 class StudentRepositry implements StudentRepositryInterface
 {
 
-      public function getAllStudents()
+      public function getAllStudent()
       {
        $students=Student::all();
-     return view('students.students',compact('students'));
+       return view('students.students',compact('students'));
       }
       public function getGender()
       {
        return Gender::all();
       }
-      public function craetestudent(){
+
+      public function craeteStudent(){
         $data['my_class']=ClassRoom::all();
         $data['parants']=MyParant::all();
         $data['Genders']=Gender::all();
@@ -47,7 +55,8 @@ class StudentRepositry implements StudentRepositryInterface
         $data['riligaion']=Religion::all();
         return view('students.add_student',$data);
       }
-      public function StudentsStore($request)
+
+      public function studentStore($request)
       {   
     
     
@@ -71,22 +80,37 @@ try{
   return $e;
 }
       }
-      public function editStudents($id){
-        return  Student::findOrfail($id);
+      public function editStudent($id)
+      {
+        $data['my_class']=ClassRoom::all();
+        $data['parants']=MyParant::all();
+        $data['Genders']=Gender::all();
+        $data['Nationals']=Nationalitie::all();
+        $data['bloods']=Bload::all();
+        $data['Grades']=Grade::all();
+        $data['sections']=Section::all();
+        $data['riligaion']=Religion::all();
+        $Student=Student::findOrfail($id);
+        return view('students.edit_student',$data,compact('Student'));
       }
-  public function StudentsUpdate($request){
-   try{
-     $Teachers= Student::findOrFail($request->id);
 
-    $Teachers->Email =$request->Email;
-    $Teachers->Password =Hash::make($request->Password);
-    $Teachers->Name =['en'=>$request->Name_en ,'ar'=>$request->Name_ar];
-    $Teachers->specialization_id =$request->specialization_id;
-    $Teachers->Gender_id =$request->Gender_id;
-    $Teachers->joining_Date =$request->joining_Date;
-    $Teachers-> Address=$request->Address;
-    $Teachers->save();
-    return redirect()->route('teacher.index');
+  public function studentsUpdate($request){
+   try{
+     $Students= Student::findOrfail($request->id);
+     $Students->Email =$request->Email;
+     $Students->Password =Hash::make($request->Password);
+     $Students->Name =['en'=>$request->Name_en ,'ar'=>$request->Name_ar];
+     $Students->nationalitie_id =$request->nationalitie_id;
+     $Students->bload_id =$request->bload_id;
+     $Students->gender_id =$request->gender_id;
+     $Students->grade_id =$request->grade_id;
+     $Students->classroom_id =$request->classroom_id;
+     $Students->parant_id=$request->parant_id;
+     $Students->academic_year=$request->academic_year;      
+     $Students->Date_Birth =$request->Date_Birth;
+     $Students-> section_id=$request->section_id;
+     $Students->save();
+     return redirect()->route('students.index');
   
    }catch(Exception $e)
 {
@@ -96,10 +120,12 @@ try{
 
   }
 
-  public function DeleteStudents($id){
-    Student::findOrfail($id)->delete();
-    
-  }
+  public function deleteStudent($request)
+  {
+     $student=Student::findOrFail($request->id);
+     $student->delete();
+     return redirect()->route('students.index');
+    }
 
 
 }
